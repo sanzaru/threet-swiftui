de89da882378e26de02d1ee5a9d10ce8287cb5d8
Martin Albrecht <sanzaru@users.noreply.github.com>
2022-07-17 12:25:04 +0200

    Better global object initialization

--- ThreeT/Sources/SceneDelegate.swift
+++ ThreeT/Sources/SceneDelegate.swift
@@ -11,15 +11,15 @@ import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    
+    // Initialize global objects
+    let settings = GameSettingsData()
+    let game = Game(mode: nil)
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        
-        // Initialize
-        let settings = GameSettings()
-        let game = Game(mode: nil)
 
         // Create the SwiftUI view that provides the window contents.
         let contentView = MainGameView()
